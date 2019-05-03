d5f6088bfa0cade9a5ce1f5d4d8be50e0b520064
enjoyer2009 <enjoyer2009@qq.com>
2019-05-03 08:33:34 +0800

    first commit

---
+++ src/main/java/com/meicloud/eshop/EurekaServer.java
@@ -0,0 +1,18 @@
+package com.meicloud.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by LUJH13 on 2019-4-23.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+
+}
