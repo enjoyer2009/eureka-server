bf00f4ee90fe6e22521dfdb0b7a49179ced524e6
enjoyer <enjoyer2009@qq.com>
2020-07-11 14:41:58 +0800

    test

--- src/main/java/com/meicloud/eshop/EurekaServer.java
+++ src/main/java/com/meicloud/eshop/EurekaServer.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServer {
 
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer.class, args);
     }
---
+++ src/main/java/com/meicloud/eshop/test.java
@@ -0,0 +1,4 @@
+package com.meicloud.eshop;
+
+public class test {
+}
